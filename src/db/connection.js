6928c95b9231120a092924de4ed7d9f750d5e970
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-06-16 18:29:21 +0300

    update: changed the pool file and added book quires

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,15 +1,5 @@
 const pool = require("./pool");
 
-// exports.dbQuery = async (queryText, queryParamss) => {
-//     return await pool.query(queryText, queryParamss);
-// };
-
-exports.dbQuery = (queryText, queryParamss) => {
-    return new Promise((resolve, reject) => {
-        pool.query(queryText, queryParamss)
-            .then((res) => {
-                resolve(res);
-            })
-            .catch((err) => reject(err));
-    });
+exports.dbQuery = async (queryText, queryParamss) => {
+    return await pool.query(queryText, queryParamss);
 };
