ce95f5213b834e92601d821b8cc8c472cfda3eca
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-06-16 18:31:23 +0300

    update: added the update and delete route

--- src/routes/bookRoute.js
+++ src/routes/bookRoute.js
@@ -4,10 +4,14 @@ let express = require("express"),
         getBooksList,
         getBookDetails,
         addBook,
+        updateBook,
+        deleteBook,
     } = require("../controller/bookController");
 
 router.get("/books", getBooksList);
 router.get("/books/details/:bookId", getBookDetails);
 router.post("/books/add", addBook);
+router.put("/books/update/:bookId", updateBook);
+router.delete("/books/delete/:bookId", deleteBook);
 
 module.exports = router;
