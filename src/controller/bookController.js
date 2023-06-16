0d0d9517fec73b8633609eb3d95a9a36a551d20b
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-06-16 18:30:37 +0300

    feat: added the update and delete functions

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,7 +1,7 @@
 const queries = require("../db/queires"),
     dbConnection = require("../db/connection");
 const router = require("../routes/storeRoute");
-const { generateStoreCode } = require("../util/helpers");
+const { idValidation } = require("../util/validation");
 
 exports.getBooksList = async (req, res) => {
     try {
@@ -71,5 +71,88 @@ exports.addBook = async (req, res) => {
     } catch (error) {
         console.log("Error in addBook", error);
         return res.status(500).send("Failed to add book");
+    }
+};
+
+exports.updateBook = async (req, res) => {
+    try {
+        let bookId = req.params.bookId;
+
+        let {
+            bookTitle,
+            bookDescription,
+            bookAuthor,
+            bookPublisher,
+            bookPages,
+            storeCode,
+        } = req.body;
+
+        let createdOn = new Date(),
+            createdBy = "admin";
+
+        if (
+            !bookId ||
+            !bookTitle ||
+            !bookAuthor ||
+            !bookPublisher ||
+            !bookPages ||
+            !storeCode
+        ) {
+            return res.status(400).send({
+                error: "Please provide bookId, title, author, puplisher, pages number",
+            });
+        }
+
+        // Validate if the bookId exists
+        const bookIdQuery = queries.queryList.SELECT_BOOK_ID_QUERY;
+        let bookExists = await idValidation(bookId, bookIdQuery);
+        if (!bookExists) {
+            return res.status(400).send("Book does not exist");
+        }
+
+        let updateBookQuery = queries.queryList.UPDATE_BOOK_QUERY;
+        await dbConnection.dbQuery(updateBookQuery, [
+            bookTitle,
+            bookDescription,
+            bookAuthor,
+            bookPublisher,
+            bookPages,
+            storeCode,
+            createdOn,
+            createdBy,
+            bookId,
+        ]);
+
+        return res.status(201).send("Book updated successfully");
+    } catch (error) {
+        console.log("Error in updateBook", error);
+        return res.status(500).send("Failed to update book");
+    }
+};
+
+exports.deleteBook = async (req, res) => {
+    try {
+        let bookId = req.params.bookId;
+
+        if (!bookId) {
+            return res.status(400).send({
+                error: "Please provide bookId",
+            });
+        }
+
+        // Validate if the bookId exists
+        const bookIdQuery = queries.queryList.SELECT_BOOK_ID_QUERY;
+        let bookExists = await idValidation(bookId, bookIdQuery);
+        if (!bookExists) {
+            return res.status(400).send("Book does not exist");
+        }
+
+        let deleteBookQuery = queries.queryList.DELETE_BOOK_QUERY;
+        await dbConnection.dbQuery(deleteBookQuery, [bookId]);
+
+        return res.status(201).send("Book deleted successfully");
+    } catch (error) {
+        console.log("Error in deleteBook", error);
+        return res.status(500).send("Failed to delete book");
     }
 };
